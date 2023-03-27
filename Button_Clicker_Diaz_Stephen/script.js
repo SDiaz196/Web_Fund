76e16cab63202eae5d528130fdea09bb7dceba80
Stephen Diaz <123036743+SDiaz196@users.noreply.github.com>
2023-03-27 16:16:25 -0400

    Add files via upload

---
+++ Button_Clicker_Diaz_Stephen/script.js
@@ -0,0 +1,16 @@
+console.log("It's Working")
+
+
+function changebtn(element) {
+    element.innerText = 'Logout';
+}
+
+function deletebtn(element) {
+    element.remove();
+} 
+
+function showPopup(msg) {
+    msg = 'Ninja was liked' 
+    alert(msg)
+}
+
