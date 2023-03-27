6796d78e22f2bc7e20a51aba4584594cb47a786a
Stephen Diaz <123036743+SDiaz196@users.noreply.github.com>
2023-03-27 16:16:35 -0400

    Add files via upload

---
+++ Likes_Diaz_Stephen/script.js
@@ -0,0 +1,23 @@
+console.log('Hi');
+
+
+var count = 9;
+function neilLike() {
+    count++
+    document.querySelector('#neil').innerText = count + 'like(s)';
+}
+
+var add = 12;
+function nicholeLike() {
+    add++
+    document.querySelector('#nichole').innerText = add + 'like(s)';
+}
+
+var plus = 9;
+function jimLike() {
+    plus++
+    document.querySelector('#jim').innerText = plus + 'like(s)';
+}
+
+
+
